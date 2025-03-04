99e3f1a46e19afe01c3cb7e6afbb002a2c740b64
Didimalang Phele <71471501+Didi779@users.noreply.github.com>
2025-03-04 13:29:40 +0200

    data_add.php

--- data_add.php
+++ data_add.php
@@ -1,33 +1,33 @@
-<?php
+<?php // Beginning of php session to store user information.
 session_start();
 if (!isset($_SESSION['username'])) {
     header("Location: index.php");
     exit();
 }
 
-$servername = "localhost";
-$username = "root"; // Change this if necessary
-$password = ""; // Change this if necessary
-$database = "blending_db"; // Make sure you have created this database
+$servername = "localhost";  //Servername is localhost because this is ran on a local server.
+$username = "root"; 
+$password = ""; //Because the database has no password, this is left blank.
+$database = "blending_db"; //Name of the created database
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);  //This is to establish the database connection.
 
-if ($conn->connect_error) {
+if ($conn->connect_error) {   //This line is there to catch errors, if the connection to the database is not established, a user should be alerted.
     die("Connection failed: " . $conn->connect_error);
 }
 
-$order_no = $_POST['order_no'];
+$order_no = $_POST['order_no'];  //Post method is a predifined method used to retrive information from the database.
 $base_tank = $_POST['base_tank'];
 $product = $_POST['product'];
 $flushed = $_POST['flushed'];
 
-$sql = "INSERT INTO blending_data (order_no, base_tank, product, flushed) VALUES ('$order_no', '$base_tank', '$product', '$flushed')";
+$sql = "INSERT INTO blending_data (order_no, base_tank, product, flushed) VALUES ('$order_no', '$base_tank', '$product', '$flushed')"; //This line is used to insert information into the table.
 
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE) {  //This statement is used to alert the browser that if a user has successfully added to the database, they should get an alert.
     echo "<script>alert('Data added successfully!'); window.location.href='dashboard.php';</script>";
-} else {
+} else {  //If adding to the database is unsuccessful, then an error message should come up.
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$conn->close();
+$conn->close(); //end of database connection.
 ?>

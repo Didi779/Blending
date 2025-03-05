e7a7ea95cfb2c880f659b760f8e285e5ef67ec38
Didimalang Phele <71471501+Didi779@users.noreply.github.com>
2025-03-05 08:51:11 +0200

    data_add.php

--- data_add.php
+++ data_add.php
@@ -1,33 +1,44 @@
-<?php // Beginning of php session to store user information.
-session_start();
+<?php 
+session_start(); // Start a new session or resume the existing session to store user information
+
+// Check if the 'username' session variable is not set
 if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
-    exit();
+    header("Location: index.php"); // Redirect the user to the index page if not logged in
+    exit(); // Stop further script execution after the redirect
 }
 
-$servername = "localhost";  //Servername is localhost because this is ran on a local server.
-$username = "root"; 
-$password = ""; //Because the database has no password, this is left blank.
-$database = "blending_db"; //Name of the created database
+// Database connection parameters
+$servername = "localhost";  // Database server name
+$username = "root"; // Database username
+$password = ""; // Database password
+$database = "blending_db"; // Database name
 
-$conn = new mysqli($servername, $username, $password, $database);  //This is to establish the database connection.
+// Create a new mysqli connection
+$conn = new mysqli($servername, $username, $password, $database); 
 
-if ($conn->connect_error) {   //This line is there to catch errors, if the connection to the database is not established, a user should be alerted.
-    die("Connection failed: " . $conn->connect_error);
+// Check for connection errors
+if ($conn->connect_error) {  
+    die("Connection failed: " . $conn->connect_error); // Terminate the script and display error message if connection fails
 }
 
-$order_no = $_POST['order_no'];  //Post method is a predifined method used to retrive information from the database.
-$base_tank = $_POST['base_tank'];
-$product = $_POST['product'];
-$flushed = $_POST['flushed'];
+// Retrieve form data from POST request
+$order_no = $_POST['order_no'];  // Get the order number from the form
+$base_tank = $_POST['base_tank']; // Get the base tank from the form
+$product = $_POST['product']; // Get the product from the form
+$flushed = $_POST['flushed']; // Get the flushed status from the form
 
-$sql = "INSERT INTO blending_data (order_no, base_tank, product, flushed) VALUES ('$order_no', '$base_tank', '$product', '$flushed')"; //This line is used to insert information into the table.
+// SQL query to insert data into the blending_data table
+$sql = "INSERT INTO blending_data (order_no, base_tank, product, flushed) VALUES ('$order_no', '$base_tank', '$product', '$flushed')"; 
 
-if ($conn->query($sql) === TRUE) {  //This statement is used to alert the browser that if a user has successfully added to the database, they should get an alert.
+// Execute the SQL query and check if it was successful
+if ($conn->query($sql) === TRUE) {  
+    // If the query was successful, display a success message and redirect to the dashboard
     echo "<script>alert('Data added successfully!'); window.location.href='dashboard.php';</script>";
-} else {  //If adding to the database is unsuccessful, then an error message should come up.
+} else { 
+    // If there was an error, display the error message
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$conn->close(); //end of database connection.
+// Close the database connection
+$conn->close(); 
 ?>
